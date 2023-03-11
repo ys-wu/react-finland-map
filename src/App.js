10f59f97485204c0ac59542b41f9c352d0adc9ac
Yusheng Wu <yusheng@Yushengs-MacBook-Pro-2.local>
2023-03-11 20:12:31 +0200

    draw circle given diameter

--- src/App.js
+++ src/App.js
@@ -5,11 +5,28 @@ const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
 
 const zoom = 11;
 
+// const Component = (props) => {
+//   const { val } = props.data;
+
+//   return <div>{ val }</div>
+// }
+
 const Component = (props) => {
   const { val } = props.data;
+  const diameter = `${ 100 * val }px`
 
-  return <div>{ val }</div>
-}
+  const circleStyle ={
+    width: diameter,
+    height: diameter,
+    borderRadius: '50%',
+    backgroundColor: 'silver',
+    opacity: 0.75,
+  };
+
+  return (
+    <div style={circleStyle}></div>
+  );
+};
 
 const data = [
   {
