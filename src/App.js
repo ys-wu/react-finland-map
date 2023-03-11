9620366d7e77cbed2c6f800412d01071d6aaa638
Yusheng Wu <yusheng@Yushengs-MacBook-Pro-2.local>
2023-03-11 18:19:13 +0200

    works with fix data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
 import Map from './Map';
 
+const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
+
+const zoom = 11;
+
+const componentFactory = (lat, lng, text) => {
+  const Component = ({ text }) => {
+    return <div>{text}</div>
+  }
+
+  return <Component lat={ lat } lng= { lng } text = { text } /> 
+};
+
+const data = [
+  {
+    city: "city1",
+    text: "My Marker",
+  },
+];
+
 function App() {
   return (
     <div className="App">
-      <Map></Map>
+      <Map
+        apiKey={ apiKey }
+        zoom={ zoom }
+        componentFactory={ componentFactory }
+        data={ data }
+      />
     </div>
   );
 }
--- src/Map.jsx
+++ src/Map.jsx
@@ -1,32 +1,36 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
-const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
-
-export default function Map(){
-  const defaultProps = {
+export default function Map({
+  apiKey,
+  zoom,
+  componentFactory,
+  data,
+}){
+  const COODINATES = {
     center: {
       lat: 10.99835602,
       lng: 77.01502627
     },
-    zoom: 11
+    city1: {
+      lat: 10.99835602,
+      lng: 77.01502627,
+    },
   };
+
+  const center = COODINATES.center;
+  const { city, text } = data[0]
+  const { lat, lng } = COODINATES[city];
 
   return (
     // Important! Always set the container height explicitly
     <div style={{ height: '100vh', width: '100%' }}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: apiKey }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
+        defaultCenter={center}
+        defaultZoom={zoom}
       >
-        <AnyReactComponent
-          lat={10.99835602}
-          lng={77.01502627}
-          text="My Marker"
-        />
+        { componentFactory(lat, lng, text)}
       </GoogleMapReact>
     </div>
   );
